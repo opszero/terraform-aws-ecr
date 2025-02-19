207cd17cd78c12c6d5e28602142403cbfe296f79
Abhi Yerra <abhi@opszero.com>
2025-02-19 11:03:50 -0800

    Added fmt

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "name" {
   description = "The name of the repository"
   type        = string
 }
+
+variable "force_delete" {
+  description = "Force delete the repository"
+  default     = false
+}
