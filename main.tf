c14a4acb4b171a41e51e8a0ced8b1c2037cfcf46
Sohan Yadav <31764946+sohanyadav@users.noreply.github.com>
2025-12-30 16:12:15 +0530

    Change countNumber from 75 to 10 in main.tf

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "aws_ecr_lifecycle_policy" "this" {
       "selection": {
         "tagStatus": "any",
         "countType": "imageCountMoreThan",
-        "countNumber": 75
+        "countNumber": 10
       },
       "action": {
         "type": "expire"
